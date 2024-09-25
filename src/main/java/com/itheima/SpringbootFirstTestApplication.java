0abaf6f4bf1163a471c9a8292c7a791a23e0c7a7
permanentct <3307617969@qq.com>
2024-09-25 16:16:20 +0800

    springboot 测试文件

---
+++ src/main/java/com/itheima/SpringbootFirstTestApplication.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFirstTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFirstTestApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/itheima/springbootfirsttest/SpringbootFirstTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.springbootfirsttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFirstTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
